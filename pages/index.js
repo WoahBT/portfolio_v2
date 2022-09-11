6cebb14eb115603767f87d8a8f11504012fc5cdb
Brian Talipan <bt@Brians-MacBook-Pro.local>
2022-09-11 18:24:47 -0400

    chore: update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,18 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>
-      Hello World. <Link href="/about">About</Link>
-    </div>
+    <main>
+        <h1 className="title">
+          Welcome to Brian's website!
+        </h1>
+
+        <p className="description">
+          xXx
+        </p>
+
+        <div className="grid">
+          <a href="https://www.instagram.com/brianxtalipan" target="_blank">don't break edge</a>
+         </div>
+    </main>
   )
 }
